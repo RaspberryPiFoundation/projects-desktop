702e423273eca0c02ddfcd590bad99272939d158
Jonic Linley <jonic@100yen.co.uk>
2017-06-26 17:27:50 +0100

    Linting

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -4,20 +4,20 @@ import PropTypes from 'prop-types'
 const Toolbar = ({
   browserHomeButtonHandler,
   dockButtonClickHandler,
-  title
-}) => 
+  title,
+}) =>
   <div className="Toolbar">
     <button>Back</button>
     <button>Forward</button>
     <button onClick={() => browserHomeButtonHandler() }>Home</button>
-    
+
     <span>{title}</span>
-    
+
     <button onClick={() => dockButtonClickHandler('left')}>Dock Left</button>
     <button onClick={() => dockButtonClickHandler('full')}>Fullscreen</button>
     <button onClick={() => dockButtonClickHandler('right')}>Dock Right</button>
   </div>
-  
+
 Toolbar.propTypes = {
   browserHomeButtonHandler: PropTypes.func.isRequired,
   dockButtonClickHandler:   PropTypes.func.isRequired,
--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -1,5 +1,5 @@
 const net = require('net')
-const port = process.env.PORT ? (process.env.PORT - 100) : 3000
+const port = process.env.PORT ? process.env.PORT - 100 : 3000 // eslint-disable-line no-magic-numbers
 
 process.env.ELECTRON_START_URL = `http://localhost:${port}`
 
@@ -7,17 +7,16 @@ const client = new net.Socket()
 const exec = require('child_process').exec
 
 let startedElectron = false
-const tryConnection = () => client.connect({port: port}, () => {
-        client.end()
-        if (!startedElectron) {
-            startedElectron = true
-            exec('npm run electron')
-        }
-    }
-);
+const tryConnection = () => client.connect({ port: port }, () => {
+  client.end()
+  if (!startedElectron) {
+    startedElectron = true
+    exec('npm run electron')
+  }
+})
 
-tryConnection();
+tryConnection()
 
-client.on('error', (error) => {
-    setTimeout(tryConnection, 100)
+client.on('error', () => {
+  setTimeout(tryConnection, 100) // eslint-disable-line no-magic-numbers
 })
